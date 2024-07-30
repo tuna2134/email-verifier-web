c2ffe6a9b7db9d3d6775fccf100cb413e064a1b2
tuna2134 <masato@priv.tuna2134.dev>
2024-07-30 09:20:58 +0000

    できた！

---
+++ app/(auth)/auth/callback/discord/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import React from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+interface User {
+    username: string;
+}
+
+export default function Page() {
+    const searchParams = useSearchParams();
+
+    let state = searchParams.get("state");
+    if (!state) {
+        return <div>Invalid state</div>;
+    }
+
+    let code = searchParams.get("code");
+    if (!code) {
+        return <div>Invalid code</div>;
+    }
+
+    const [user, setUser] = React.useState<User | null>(null);
+
+    React.useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/verify/discord`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                code,
+                state,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.status === 200) {
+                setUser(data.user);
+            }
+        });
+    }, [setUser, state, code]);
+
+    return (
+        <div className="h-screen">
+            <div className="flex w-full h-full items-center justify-center">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>認証確認画面</CardTitle>     
+                        <CardDescription>あなたのメールアドレスが有効かどうか確認しています。</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>{user ? `${user.username}さん、認証に成功しました。` : "認証中です"}</p>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    )
+}
---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+import Link from "next/link";
+
+export default function Page({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    let code = searchParams.code;
+    if (!code) {
+        return <div>Invalid code</div>;
+    }
+    return (
+        <div className="h-screen">
+            <div className="flex w-full h-full items-center justify-center">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>ログイン</CardTitle>
+                        <CardDescription>ログインすることでメールアドレスをチェックします。</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Button asChild>
+                            <Link href={`${process.env.NEXT_PUBLIC_DISCORD_OAUTH_URL}&state=${code}`}>Login with Discord</Link>
+                        </Button>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    )
+}
