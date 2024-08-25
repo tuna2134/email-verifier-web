bd7addffc60e295c71e0cb17a96e81948d589667
tuna2134 <masato@priv.tuna2134.dev>
2024-08-25 10:05:36 +0000

    ok

--- components/pages/dashboard.tsx
+++ components/pages/dashboard.tsx
@@ -84,7 +84,7 @@ export function DashboardLayout({ children, navs }: DashboardLayoutProps) {
                             Home
                         </Link>
                     </Button>
-                    <div className="border-b" />
+                    {navs.length !== 0 && <div className="border-b" />}
                     {navs.map((nav, index) => (
                         <Button asChild variant="ghost" size="lg">
                             <Link
